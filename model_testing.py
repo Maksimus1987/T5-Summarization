d15df0fde0588ce1c3e7af53b2eeb4c59257f0ef
Maksimus1987 <149474821+Maksimus1987@users.noreply.github.com>
2024-04-09 21:09:09 +0300

    Update model_testing.py
    Оформление кода в соответствии с PEP8

--- model_testing.py
+++ model_testing.py
@@ -1,18 +1,25 @@
-from sklearn.linear_model import LinearRegression # Импорт модели линейной регрессии
-from sklearn.model_selection import train_test_split # Импорт функции для разделения данных на обучающий и тестовый наборы
-from sklearn.metrics import mean_squared_error # Импорт функции для расчета среднеквадратичной ошибки
-import pandas as pd # Импорт библиотеки pandas для работы с данными
+from sklearn.linear_model import LinearRegression  # Импорт модели линейной регрессии
+from sklearn.model_selection import (
+    train_test_split,
+)  # Импорт функции для разделения данных на обучающий и тестовый наборы
+from sklearn.metrics import (
+    mean_squared_error,
+)  # Импорт функции для расчета среднеквадратичной ошибки
+import pandas as pd  # Импорт библиотеки pandas для работы с данными
+
 
 def prepare_and_train_model(train_data_path):
-    data = pd.read_csv(train_data_path) # Загрузка данных из CSV файла
-    
+    data = pd.read_csv(train_data_path)  # Загрузка данных из CSV файла
+
     # Подготовка признаков и целевой переменной
-    X = data['Day'].values.reshape(-1, 1)
-    y = data['Temperature']
-    
+    X = data["Day"].values.reshape(-1, 1)
+    y = data["Temperature"]
+
     # Разделение данных на обучающий и тестовый наборы
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
     # Обучение модели линейной регрессии
     model = LinearRegression()
     model.fit(X_train, y_train)
@@ -20,23 +27,26 @@ def prepare_and_train_model(train_data_path):
     # Прогнозирование значений на тестовом наборе и расчет среднеквадратичной ошибки
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
-    print(f'MSE: {mse}')
+    print(f"MSE: {mse}")
     return model
 
+
 # Обучение модели на предобработанных обучающих данных
-model = prepare_and_train_model('train/preprocessed_train.csv')
+model = prepare_and_train_model("train/preprocessed_train.csv")
+
 
 def test_model(model, test_data_path):
-    data = pd.read_csv(test_data_path) # Загрузка тестовых данных из CSV файла
+    data = pd.read_csv(test_data_path)  # Загрузка тестовых данных из CSV файла
 
     # Подготовка признаков и целевой переменной
-    X = data['Day'].values.reshape(-1, 1)
-    y = data['Temperature']
+    X = data["Day"].values.reshape(-1, 1)
+    y = data["Temperature"]
 
     # Прогнозирование значений на тестовом наборе и расчет среднеквадратичной ошибки
     y_pred = model.predict(X)
     mse = mean_squared_error(y, y_pred)
-    print(f'Test MSE: {mse}')
+    print(f"Test MSE: {mse}")
+
 
 # Проверка модели на предобработанных тестовых данных
-test_model(model, 'test/preprocessed_test.csv')
+test_model(model, "test/preprocessed_test.csv")
