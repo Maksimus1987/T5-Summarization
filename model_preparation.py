b7c11c6a1dce87b4aad9bd0669e8e5d5bff2e85d
Maksimus1987 <149474821+Maksimus1987@users.noreply.github.com>
2024-04-09 21:11:12 +0300

    Update model_preparation.py
    Добавление комментариев к коду

--- model_preparation.py
+++ model_preparation.py
@@ -1,20 +1,20 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-import pandas as pd
+from sklearn.linear_model import LinearRegression  # Импорт модели линейной регрессии
+from sklearn.model_selection import train_test_split  # Импорт функции для разделения данных на обучающий и тестовый наборы
+from sklearn.metrics import mean_squared_error  # Импорт функции для расчета среднеквадратичной ошибки
+import pandas as pd  # Импорт библиотеки pandas для работы с данными
 
 def prepare_and_train_model(train_data_path):
-    data = pd.read_csv(train_data_path)
-    X = data['Day'].values.reshape(-1, 1)
-    y = data['Temperature']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    data = pd.read_csv(train_data_path)  # Загрузка обучающих данных из CSV файла
+    X = data['Day'].values.reshape(-1, 1)  # Выделение признаков
+    y = data['Temperature']  # Выделение целевой переменной
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # Разделение данных на обучающий и тестовый наборы
+    
+    model = LinearRegression()  # Создание модели линейной регрессии
+    model.fit(X_train, y_train)  # Обучение модели на обучающем наборе
+    
+    y_pred = model.predict(X_test)  # Прогнозирование значений на тестовом наборе
+    mse = mean_squared_error(y_test, y_pred)  # Вычисление среднеквадратичной ошибки
+    print(f'MSE: {mse}')  # Вывод значения среднеквадратичной ошибки
+    return model  # Возвращение обученной модели
 
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    print(f'MSE: {mse}')
-    return model
-
-model = prepare_and_train_model('train/preprocessed_train.csv')
+model = prepare_and_train_model('train/preprocessed_train.csv')  # Обучение модели на предобработанных обучающих данных
